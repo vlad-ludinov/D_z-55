0f80754c3bc857a71583f3380f4bfa2da84d6047
vlad <vladislavludinov3006@gmail.com>
2023-08-18 17:29:56 +0300

    task2 complete

--- task2.py
+++ task2.py
@@ -1,27 +1,9 @@
 from need import list_gen
 from need import algo_time
-
-def quick_sort(lst):
-    if len(lst) <= 1:
-        return lst
-    pivot = lst[len(lst) // 2]
-    left = list(filter(lambda x: x < pivot, lst))
-    center = [i for i in lst if i == pivot]
-    right = list(filter(lambda x: x > pivot, lst))
-    return quick_sort(left) + center + quick_sort(right)
+from need import quick_sort
+from need import counting_sort
 
 
-def counting_sort(list):
-    max_item = max(list)
-    min_item = min(list)
-    lst=[0 for _ in range(max_item - min_item + 1)]
-    for i in list:
-        lst[i - min_item] = lst[i - min_item]+1
-    res_list = []
-    for i in range(len(lst)):
-        if lst[i]:
-            res_list.extend([i + min_item] * lst[i])
-    return res_list
 
 
 # получается что сложность быстрой сортировки O(n * log n),
